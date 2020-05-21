00c09efc2c898cac990ab14333a711192156295a
r9hino <philippe.ilharreguy@gmail.com>
2020-05-21 15:06:15 -0400

    Add routes to App.

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import {Navbar, Nav} from 'react-bootstrap';
 import socketIOClient from 'socket.io-client';
 import Monitor from './Monitor';
@@ -34,29 +35,39 @@ function App() {
   }, []);
   
   return (
+    <Router>
     <div className="App">
       <header>
-        <Navbar expand="sm" variant="dark" bg="dark">
-          <Navbar.Brand href="#home">BBB Fabrica</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="#monitor">Monitor</Nav.Link>
-                  <Nav.Link href="#inputoutput">IO</Nav.Link>
-              </Nav>
-          </Navbar.Collapse>
+        <Navbar variant="dark" bg="dark">
+          <Navbar.Brand style={{marginRight:'35px'}} href="/">BBB Fabrica</Navbar.Brand>
+          
+            
+            <Nav>
+              <Link style={linkStyle} to="/">Monitor</Link>
+              <Link style={linkStyle} to="/inputoutput">IO</Link>
+            </Nav>
+          
         </Navbar>
       </header>
-      
-      <div className="container-fluid">
-        <Monitor staticSystemData={staticSystemData} dynamicSystemData={dynamicSystemData}/>
-        <InputOutput analogValues={analogValues}/>
-        
-      </div>
+        <div className="container-fluid">
+          <Switch>
+            <Route path="/inputoutput">
+              <InputOutput analogValues={analogValues}/>
+            </Route>
+            <Route path="/">
+              <Monitor staticSystemData={staticSystemData} dynamicSystemData={dynamicSystemData} />
+            </Route>
+          </Switch>
+        </div>
     </div>
+    </Router>
   );
 }
 export default App;
 
-
+const linkStyle = {
+    color: '#afafaf',
+    textDecoration: 'none',
+    marginRight: '15px'
+}
 
